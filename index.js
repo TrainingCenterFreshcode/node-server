252eb591a7c0f7e6172688a2b6cb3e96a2549ad6
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-04-12 19:44:32 +0300

    Server side rendering example

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const http = require('http');
+const fs = require('fs/promises');
 
 const PORT = 5000;
 
 const requestListener = (request, response) => {
-    const { url, method } = request;
-    console.log(url, method);
-    
-    response.statusCode = 404;
-    response.end('Hello from server');
+    const { url } = request;
+
+    if(url === '/index.html') { // http://localhost:5000/index.html
+        fs.readFile('./views/index.html', 'utf-8')
+        .then(data => {
+            response.statusCode = 200;
+            response.end(data);
+        });
+    } else {
+        response.statusCode = 404;
+        response.end();
+    }
 }
 const server = http.createServer(requestListener);
 
