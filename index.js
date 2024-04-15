d87e54010f78cc0255310f8426d67ab672abd6aa
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-04-15 20:55:12 +0300

    Model + Controller

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { validateUser } = require('./middlewares');
+const UserController = require('./controllers/UserController');
 const app = express();
 
 const bodyParser = express.json(); // request.body
 
 const PORT = 5000;
 
-app.post('/user', bodyParser, validateUser);
+// роут на реєстрацію
+app.post('/user', bodyParser, validateUser, UserController.registerUser);
+// роут на отримання всіх користувачів
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
@@ -25,8 +28,8 @@ app.listen(PORT, () => {
 + 1. Прийняти запит на певний роут
 + 2. Розпарсити дані, які прийшли з запитом
 + 3. Перевірити (валідувати) дані
-4. Зберегти ці дані
-5. Надіслати відповідь клієнту
++ 4. Зберегти ці дані
++ 5. Надіслати відповідь клієнту
 
 
 */
