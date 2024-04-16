fcbc12103df6c124bf50bcb2acebdc740aac9854
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-04-16 18:58:45 +0300

    Request params

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -18,5 +18,12 @@ module.exports.getAllUsers = (req, res, next) => {
 
 // метод контроллера на отримання конкретного користувача
 module.exports.getOneUser = (req, res, next) => {
-    console.log(req.query);
+    const { userId } = req.params;
+    const user = User.findOne(Number(userId));
+
+    if(user) { // якщо юзер існує, тобто, якщо він не undefined
+        res.status(200).send(user);
+    } else {
+        res.status(404).end();
+    }
 }
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post('/user', bodyParser, validateUser, UserController.registerUser);
 // роут на отримання всіх користувачів
 app.get('/users', UserController.getAllUsers); // http://localhost:5000/users
 // роут на отримання якогось конкретного юзера
-app.get('/user', UserController.getOneUser);
+app.get('/user/:userId', UserController.getOneUser);
 
 
 app.listen(PORT, () => {
